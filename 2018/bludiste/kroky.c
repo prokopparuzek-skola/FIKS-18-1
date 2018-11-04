7e773be4bc7a035ed07960844a95905e89dc357b
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-11-04 19:02:29 +0100

    bludiste 0.2

--- 2018/bludiste/kroky.c
+++ 2018/bludiste/kroky.c
@@ -1,17 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bludiste.h"
-#include "bludiste.h"
 
 blud storeBlud(void);
 int kroky(blud);
 void printBlud(blud);
 bod *availableSteps(bod);
 bod storeStep(int x, int y);
+bod *noWalls(blud *maze, bod *steps);
+int makeStep(blud *maze, queue* stack);
+int solve(blud maze);
 
 int main() {
-//   printf("%d\n", kroky(storeBlud()));
-    printBlud(storeBlud());
+    blud maze = storeBlud();
+    printBlud(maze);
+    solve(maze);
+}
+
+int solve(blud maze) {
+   int sizeQ = 128;
+   queue stack = {0, 0, sizeQ, NULL};
+   stack.stepQueue = calloc(sizeQ, sizeof(step));
+   stack.stepQueue[0].where.x = 0;
+   stack.stepQueue[0].where.y = 0;
+   stack.stepQueue[0].depth = 0;
+   stack.stepQueue[0].parent = 0;
+   int count;
+   while ((count = makeStep(&maze, &stack)) == -1);
+   printf("%d\n",count);
+}
+
+int makeStep(blud *maze, queue* stack) {
+    int i, j, r = stack->right;
+    bod *steps;
+    for (i = stack->left; i <= stack->right; i++) {
+        steps = noWalls(maze, availableSteps(stack->stepQueue[i].where));
+        for (j = 0; j < 4; j++) {
+            if (steps[j].x == -1) continue;
+            else {
+                r++;
+                if (r >= stack->size) stack->stepQueue = realloc(stack->stepQueue, stack->size* 2 * sizeof(step));
+                stack->stepQueue[r].where.x = steps[j].x;
+                stack->stepQueue[r].where.y = steps[j].y;
+                stack->stepQueue[r].depth = stack->stepQueue[stack->left].depth ? stack->stepQueue[stack->left - 1].depth + 1 : 1;
+                stack->stepQueue[r].parent = i;
+                if (steps[j].x == (maze->x - 1) && steps[j].y == (maze-> y - 1)) return r;
+            }
+        }
+    }
+    stack->left = stack->right + 1;
+    stack->right = r;
+    return -1;
 }
 
 blud storeBlud() {
@@ -27,12 +66,6 @@ blud storeBlud() {
     return maze;
 }
 
-int kroky(blud maze) {
-    queue steps = {0, 0, 128, NULL};
-    steps.stepQueue = malloc(sizeof(step) * steps.size);
-
-}
-
 void printBlud(blud maze) {
     int i, j;
     for (i = 0; i < maze.y; i++) {
@@ -45,17 +78,17 @@ void printBlud(blud maze) {
 
 bod *availableSteps(bod where) {
     bod *steps = malloc(sizeof(bod) * 4);
-    steps[0] = storeStep(where.x, where.y -1);
-    steps[1] = storeStep(where.x + where.x, where.y);
-    steps[2] = storeStep(where.x, where.y +1);
-    steps[3] = storeStep(where.x -1, where.y);
+    steps[0] = storeStep(where.x, where.y - 1);
+    steps[1] = storeStep(where.x + 1, where.y);
+    steps[2] = storeStep(where.x, where.y + 1);
+    steps[3] = storeStep(where.x - 1, where.y);
     return steps;
 }
 
 bod *noWalls(blud *maze, bod *steps) {
     int i;
     for (i = 0; i < 4; i++) {
-        if (steps[i].x < 0 || steps[i].y < 0) {
+        if (steps[i].x < 0 || steps[i].y < 0 || steps[i].x >= maze->x || steps[i].y >= maze-> y) {
             (steps + i)->x = -1;
             (steps + i)->y = -1;
             continue;
@@ -65,6 +98,7 @@ bod *noWalls(blud *maze, bod *steps) {
             (steps + i)->y = -1;
         }
     }
+    return steps;
 }
 
 bod storeStep(int x, int y) {
