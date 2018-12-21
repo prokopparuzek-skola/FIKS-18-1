083bb16884bab77e1af2b7096ffc8c172708a001
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2018-12-22 00:07:41 +0100

    vicevlakna

--- 2018/cenzori/cenzor.go
+++ 2018/cenzori/cenzor.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func interval(ID []uint, posun int) {
+func interval(ID []uint, posun int, task chan uint) {
 	min := ID[0]
 	maxID := ID[0]
 	maxI := 0
@@ -14,35 +14,48 @@ func interval(ID []uint, posun int) {
 			min = id
 		}
 	}
-	fmt.Printf("%v\n", min)
+	//fmt.Printf("%v\n", min)
+	task <- min
 	for i, id := range ID {
 		if id > maxID {
 			maxID = id
 			maxI = i
 		}
 	}
-	fmt.Printf("%d\n", maxI+posun)
+	//fmt.Printf("%d\n", maxI+posun)
+	task <- uint(maxI + posun)
 	for _, id := range ID {
 		XOR ^= id
 	}
-	fmt.Printf("%d\n", XOR)
+	//fmt.Printf("%d\n", XOR)
+	task <- XOR
 }
 
 func main() {
 	var T, N, Q, B, E uint
 	var cenzor uint
+	var tasks []chan uint
+
 	fmt.Scanf("%d", &T)
 	for i := uint(0); i < T; i++ {
 		var ID []uint
 		fmt.Scanf("%d%d", &N, &Q)
+		for j := uint(0); j < Q; j++ {
+			tasks = append(tasks, make(chan uint, 3))
+		}
 		for ; N > 0; N-- {
 			fmt.Scanf("%d", &cenzor)
 			ID = append(ID, cenzor)
 		}
-		for ; Q > 0; Q-- {
+		for j := uint(0); j < Q; j++ {
 			fmt.Scanf("%d%d", &B, &E)
-			//go interval(ID[B:E+1], int(B))
-			interval(ID[B:E+1], int(B))
+			//interval(ID[B:E+1], int(B))
+			go interval(ID[B:E+1], int(B), tasks[j])
+		}
+		for j := uint(0); j < Q; j++ {
+			for k := 0; k < 3; k++ {
+				fmt.Printf("%d\n", <-tasks[j])
+			}
 		}
 	}
 }
