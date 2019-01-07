e3744db72a7f7a66cacafc68ed604e84616e192e
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-01-07 16:52:08 +0100

    paska0.2

--- 2018/paska/paska.c
+++ 2018/paska/paska.c
@@ -16,6 +16,7 @@ int compare (const void *first, const void *twice) {
 double compute (void) {
     unsigned K, i, IH = 0, ID = 0;
     bod *stack, *D, *H;
+    char determinant = 0;
 
     scanf("%u", &K);
     stack = malloc(K * sizeof(bod));
@@ -33,7 +34,15 @@ double compute (void) {
     H[0] = stack[0];
     D[0] = stack[0];
     for (i = 1; i < K; i++) {
-
+        if (IH >= 2) {
+            determinant = (H[IH].x - H[IH - 1].x) * (stack[i].y - H[IH].y) - (stack[i].x - H[IH].x) * (H[IH].y - (H[IH - 1].y)); // Počítá determinant, směr úhlu
+        }
+        while (IH >= 2 && determinant) { // Horní obálka
+            IH--;
+            determinant = (H[IH].x - H[IH - 1].x) * (stack[i].y - H[IH].y) - (stack[i].x - H[IH].x) * (H[IH].y - (H[IH - 1].y)); // Počítá determinant, směr úhlu
+        }
+        IH++;
+        H[IH] = stack[i];
     }
 }
 
