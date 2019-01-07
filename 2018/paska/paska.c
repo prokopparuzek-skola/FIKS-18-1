461a9314b9ed3415b6017aa7a9e9fac8952a5e6f
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-01-07 18:26:31 +0100

    paska0.5 - mam body

--- 2018/paska/paska.c
+++ 2018/paska/paska.c
@@ -13,23 +13,19 @@ int compare (const void *first, const void *twice) {
     }
 }
 
-double compute (void) {
-    unsigned K, i, IndexHorni = 0, IndexDolni = 0;
-    bod *stack, *Dolni, *Horni;
+double compute (bod *stack, unsigned K) {
+    unsigned i, IndexHorni = 0, IndexDolni = 0;
+    bod *Dolni, *Horni;
     int determinant = 0;
 
-    scanf("%u", &K);
-    stack = malloc(K * sizeof(bod));
     Horni =  malloc(K * sizeof(bod));
     Dolni = malloc(K * sizeof(bod));
 
-    if (stack == NULL || Horni == NULL || Dolni == NULL) {
+    if (Horni == NULL || Dolni == NULL) {
         puts("Málo paměti!");
         exit(1);
     }
-    for (i = 0; i < K; i++) {
-        scanf("%d %d", &stack[i].x, &stack[i].y);
-    }
+
     qsort(stack, K, sizeof(bod), compare);
     Horni[0] = stack[0];
     Dolni[0] = stack[0];
@@ -38,7 +34,7 @@ double compute (void) {
         if (IndexHorni >= 1) { // Horní obálka
             determinant = (Horni[IndexHorni].x - Horni[IndexHorni - 1].x) * (stack[i].y - Horni[IndexHorni].y) - (stack[i].x - Horni[IndexHorni].x) * (Horni[IndexHorni].y - (Horni[IndexHorni - 1].y)); // Počítá determinant, směr úhlu
         }
-        while (IndexHorni >= 1 && determinant > 0) {
+        while (IndexHorni >= 1 && determinant >= 0) {
             IndexHorni--;
             if (IndexHorni >= 1) {
                 determinant = (Horni[IndexHorni].x - Horni[IndexHorni - 1].x) * (stack[i].y - Horni[IndexHorni].y) - (stack[i].x - Horni[IndexHorni].x) * (Horni[IndexHorni].y - (Horni[IndexHorni - 1].y)); // Počítá determinant, směr úhlu
@@ -50,7 +46,7 @@ double compute (void) {
         if (IndexDolni >= 1) { // Dolní obálka
             determinant = (Dolni[IndexDolni].x - Dolni[IndexDolni - 1].x) * (stack[i].y - Dolni[IndexDolni].y) - (stack[i].x - Dolni[IndexDolni].x) * (Dolni[IndexDolni].y - (Dolni[IndexDolni - 1].y)); // Počítá determinant, směr úhlu
         }
-        while (IndexDolni >= 1 && determinant < 0) {
+        while (IndexDolni >= 1 && determinant <= 0) {
             IndexDolni--;
             if (IndexDolni >= 1) {
                 determinant = (Dolni[IndexDolni].x - Dolni[IndexDolni - 1].x) * (stack[i].y - Dolni[IndexDolni].y) - (stack[i].x - Dolni[IndexDolni].x) * (Dolni[IndexDolni].y - (Dolni[IndexDolni - 1].y)); // Počítá determinant, směr úhlu
@@ -69,10 +65,31 @@ double compute (void) {
 }
 
 int main () {
-    unsigned i, N;
+    unsigned i, j, N, K, SizeStack = 32, IndexStack = 0;
+    bod *stack;
+
+    stack = malloc(sizeof(bod) * BLOCK);
+    if (stack == NULL) {
+        puts("Málo paměti!");
+        exit(1);
+    }
+
     scanf("%u", &N);
     for (i = 0; i < N; i++) {
-        compute();
+        scanf("%u", &K);
+        if ((IndexStack + K) > SizeStack) {
+            stack = realloc(stack, sizeof(bod) * SizeStack + BLOCK);
+            SizeStack += BLOCK;
+            if (stack == NULL) {
+                puts("Málo paměti!");
+                exit(1);
+            }
+        }
+        for (j = 0; j < K; j++) {
+            scanf("%d %d", &stack[IndexStack + j].x, &stack[IndexStack + j].y);
+        }
+        IndexStack += K;
     }
+    compute(stack, IndexStack);
     return 0;
 }
--- 2018/paska/paska.h
+++ 2018/paska/paska.h
@@ -1,9 +1,11 @@
 #ifndef __PASKA
 #define __PASKA
+#define BLOCK 32
+
 typedef struct {
     int x;
     int y;
 } bod;
 
-double compute (void);
+double compute (bod*, unsigned);
 #endif /*__PASKA*/
