787b94506bcb267a8fa10936ea129a2c5f6f86f1
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-01-07 16:58:02 +0100

    detreminant1

--- 2018/paska/paska.c
+++ 2018/paska/paska.c
@@ -34,16 +34,25 @@ double compute (void) {
     H[0] = stack[0];
     D[0] = stack[0];
     for (i = 1; i < K; i++) {
-        if (IH >= 2) {
+        if (IH >= 2) { // Horní obálka
             determinant = (H[IH].x - H[IH - 1].x) * (stack[i].y - H[IH].y) - (stack[i].x - H[IH].x) * (H[IH].y - (H[IH - 1].y)); // Počítá determinant, směr úhlu
         }
-        while (IH >= 2 && determinant) { // Horní obálka
+        while (IH >= 2 && determinant > 0) {
             IH--;
             determinant = (H[IH].x - H[IH - 1].x) * (stack[i].y - H[IH].y) - (stack[i].x - H[IH].x) * (H[IH].y - (H[IH - 1].y)); // Počítá determinant, směr úhlu
         }
         IH++;
         H[IH] = stack[i];
     }
+        if (ID >= 2) { // Dolní obálka
+            determinant = (D[ID].x - D[ID - 1].x) * (stack[i].y - D[ID].y) - (stack[i].x - D[ID].x) * (D[ID].y - (D[ID - 1].y)); // Počítá determinant, směr úhlu
+        }
+        while (ID >= 2 && determinant < 0) {
+            ID--;
+            determinant = (D[ID].x - D[ID - 1].x) * (stack[i].y - D[ID].y) - (stack[i].x - D[ID].x) * (D[ID].y - (D[ID - 1].y)); // Počítá determinant, směr úhlu
+        }
+        ID++;
+        D[ID] = stack[i];
 }
 
 int main () {
