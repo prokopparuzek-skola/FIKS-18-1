8d39b042e1b8647336545f546e0464dfcf36d322
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-01-07 17:41:04 +0100

    paska0.4

--- 2018/paska/paska.c
+++ 2018/paska/paska.c
@@ -14,16 +14,16 @@ int compare (const void *first, const void *twice) {
 }
 
 double compute (void) {
-    unsigned K, i, IH = 0, ID = 0;
-    bod *stack, *D, *H;
-    char determinant = 0;
+    unsigned K, i, IndexHorni = 0, IndexDolni = 0;
+    bod *stack, *Dolni, *Horni;
+    int determinant = 0;
 
     scanf("%u", &K);
     stack = malloc(K * sizeof(bod));
-    H = malloc(K * sizeof(bod));
-    D = malloc(K * sizeof(bod));
+    Horni =  malloc(K * sizeof(bod));
+    Dolni = malloc(K * sizeof(bod));
 
-    if (stack == NULL || H == NULL || D == NULL) {
+    if (stack == NULL || Horni == NULL || Dolni == NULL) {
         puts("Málo paměti!");
         exit(1);
     }
@@ -31,34 +31,41 @@ double compute (void) {
         scanf("%d %d", &stack[i].x, &stack[i].y);
     }
     qsort(stack, K, sizeof(bod), compare);
-    H[0] = stack[0];
-    D[0] = stack[0];
+    Horni[0] = stack[0];
+    Dolni[0] = stack[0];
 
     for (i = 1; i < K; i++) {
-        if (IH >= 2) { // Horní obálka
-            determinant = (H[IH].x - H[IH - 1].x) * (stack[i].y - H[IH].y) - (stack[i].x - H[IH].x) * (H[IH].y - (H[IH - 1].y)); // Počítá determinant, směr úhlu
+        if (IndexHorni >= 1) { // Horní obálka
+            determinant = (Horni[IndexHorni].x - Horni[IndexHorni - 1].x) * (stack[i].y - Horni[IndexHorni].y) - (stack[i].x - Horni[IndexHorni].x) * (Horni[IndexHorni].y - (Horni[IndexHorni - 1].y)); // Počítá determinant, směr úhlu
         }
-        while (IH >= 2 && determinant > 0) {
-            IH--;
-            determinant = (H[IH].x - H[IH - 1].x) * (stack[i].y - H[IH].y) - (stack[i].x - H[IH].x) * (H[IH].y - (H[IH - 1].y)); // Počítá determinant, směr úhlu
+        while (IndexHorni >= 1 && determinant > 0) {
+            IndexHorni--;
+            if (IndexHorni >= 1) {
+                determinant = (Horni[IndexHorni].x - Horni[IndexHorni - 1].x) * (stack[i].y - Horni[IndexHorni].y) - (stack[i].x - Horni[IndexHorni].x) * (Horni[IndexHorni].y - (Horni[IndexHorni - 1].y)); // Počítá determinant, směr úhlu
+            }
         }
-        IH++;
-        H[IH] = stack[i];
-        if (ID >= 2) { // Dolní obálka
-            determinant = (D[ID].x - D[ID - 1].x) * (stack[i].y - D[ID].y) - (stack[i].x - D[ID].x) * (D[ID].y - (D[ID - 1].y)); // Počítá determinant, směr úhlu
+        IndexHorni++;
+        Horni[IndexHorni] = stack[i];
+
+        if (IndexDolni >= 1) { // Dolní obálka
+            determinant = (Dolni[IndexDolni].x - Dolni[IndexDolni - 1].x) * (stack[i].y - Dolni[IndexDolni].y) - (stack[i].x - Dolni[IndexDolni].x) * (Dolni[IndexDolni].y - (Dolni[IndexDolni - 1].y)); // Počítá determinant, směr úhlu
         }
-        while (ID >= 2 && determinant < 0) {
-            ID--;
-            determinant = (D[ID].x - D[ID - 1].x) * (stack[i].y - D[ID].y) - (stack[i].x - D[ID].x) * (D[ID].y - (D[ID - 1].y)); // Počítá determinant, směr úhlu
+        while (IndexDolni >= 1 && determinant < 0) {
+            IndexDolni--;
+            if (IndexDolni >= 1) {
+                determinant = (Dolni[IndexDolni].x - Dolni[IndexDolni - 1].x) * (stack[i].y - Dolni[IndexDolni].y) - (stack[i].x - Dolni[IndexDolni].x) * (Dolni[IndexDolni].y - (Dolni[IndexDolni - 1].y)); // Počítá determinant, směr úhlu
+            }
         }
-        ID++;
-        D[ID] = stack[i];
+        IndexDolni++;
+        Dolni[IndexDolni] = stack[i];
     }
 
-    for (i = 0; i < IH; i++)
-        printf("%d %d\n", H[IH].x, H[IH].y);
-    for (i = 0; i < ID; i++)
-        printf("%d %d\n", D[ID].x, D[ID].y);
+    puts("Horni");
+    for (i = 0; i <= IndexHorni; i++)
+        printf("%d %d\n", Horni[i].x, Horni[i].y);
+    puts("Dolni");
+    for (i = 0; i <= IndexDolni; i++)
+        printf("%d %d\n", Dolni[i].x, Dolni[i].y);
 }
 
 int main () {
