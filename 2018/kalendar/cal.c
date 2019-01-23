ef7e26d2cd45a6a8d15d9c13088dc394619f96c8
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-01-23 18:46:25 +0100

    Merge into one file,k

--- 2018/kalendar/cal.c
+++ 2018/kalendar/cal.c
@@ -2,7 +2,35 @@
  * Nefunguje pro data před 20.8. 1984
  */
 #include <stdio.h>
-#include "kalendar.h"
+
+#define MVR 15 //Měsíce v roce
+#define DVT 9 //Dny v týdnu
+#define PRM 3 //Přestupný měsíc
+#define PRR 3 //Přestupný rok
+#define VPR 100 //Výjimky z přestupného roku
+#define DVR 350 // Dny v roce
+enum {SD = 20, SM = 8, SY = 1984}; // Určuje počáteční datum; SD:den, SM:měsíc, SY:rok
+
+#define MVRGK 12
+#define PMGK 2 // Přestupný měsíc gregoriánský kalendář
+#define PRGK 4 // Přestupný rok gregoriánský kalendář
+#define VPRGK 100 // Výjimka z přestupného roku gregoriánský kalendář
+#define VZVPRGK 400 // Výjimka z výjimky přestupného roku gregoriánský kalendář
+#define DVRGK 365 // Dny v roce gregoriánský kalendář
+
+static int DVMGK[] = {31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // Měsíce v roce 1 a poslední je prosinec
+static int DVMVK[] = {-1, 25, 21, 21, 24, 24, 25, 25, 21, 25, 24, 21, 24, 21, 24, 25}; // Měsíce v roce, nový kalendář
+
+typedef struct { // Uložení datumu
+    int day;
+    int dayOfWeek;
+    int month;
+    int year;
+} date;
+
+void prepocti(date*);
+int spoctiDny (date* from);
+date *spoctiDatum (int days, date* to);
 
 void prepocti(date *from) { // Přepočítá datum
     int days;
