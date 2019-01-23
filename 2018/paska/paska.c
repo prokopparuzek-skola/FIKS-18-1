34fefd493778ebc56de73e6e93b42b4c39af1794
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-01-23 18:44:46 +0100

    Merge into one file

--- 2018/paska/paska.c
+++ 2018/paska/paska.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "paska.h"
+
+#define BLOCK 32
+
+typedef struct {
+    int x;
+    int y;
+} bod;
+typedef struct { // Obsahuje obě obálky i s indexy
+    unsigned IndexHorni;
+    unsigned IndexDolni;
+    bod *Horni;
+    bod *Dolni;
+} envelop;
+typedef struct { // pouze 1 obálka
+    unsigned Index;
+    bod *stack;
+} magazine;
+
+static int compare (const void *first, const void *twice);
+magazine points (bod*, unsigned); // Vrací body na obaly, 1 dvakrát
+double compute (bod *stack, unsigned IndexStack); // Spočítá vzdálenosti mezi body, vrací výsledek
 
 static int compare (const void *first, const void *twice) { // Porovnává body
     bod *one = (bod*) first;
